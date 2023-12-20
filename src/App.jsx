6647cd78b678bbb38dc6ebc5bd2051e74353fade
Yasai <alexvidalcasado@gmail.com>
2023-12-20 08:32:00 +0100

    [UPDATE] Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import axios from "axios";
 import { AppRoutes } from "./router/AppRoutes";
+import { useEffect } from "react";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8080";
