a4ab8a2bcba43e211f00dd109b194559c5beff4a
PauGallego <paugf2004@gmail.com>
2024-01-13 20:50:32 +0100

    responsive 1srt commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,21 +16,21 @@ export const NavBar = () => {
 
   return (
     <nav className="bg-primary p-5 border-b-2 border-black">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold flex items-center">
+      <div className="container mx-auto flex justify-between items-center flex-col md:flex-row">
+        <div className="text-white font-bold flex items-center ">
           <NavLink to="/config">
             <img src={`${photo}`} className="w-14 rounded-full" alt="" />
           </NavLink>
           <p className="ms-5 font-nds text-2xl">{user.nom}</p>
         </div>
         <ul className="flex space-x-4">
-          <li className="text-white font-nds text-2xl">
+          <li className="text-white font-nds md:text-2xl text-4xl">
             <NavLink to="/dashboard">Inicio</NavLink>
           </li>
-          <li className="text-white font-nds text-2xl">
+          <li className="text-white font-nds md:text-2xl text-4xl">
             <NavLink to="/students">Alumnos</NavLink>
           </li>
-          <li className="text-white font-nds text-2xl">
+          <li className="text-white font-nds md:text-2xl text-4xl">
             <NavLink to="/login" onClick={logout}>
               Cerrar sesión
             </NavLink>
